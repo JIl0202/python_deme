0e48a4a5644bf9dd409458fa4b5ed7cfe4d016ed
yuhongyangl_home <Yu420117@icloud.com>
2023-03-30 22:06:39 +0800

    chatgpt 优化openai
    Signed-off-by: yuhongyangl_home <Yu420117@icloud.com>

--- test_auto/tests/test.py
+++ test_auto/tests/test.py
@@ -3,15 +3,27 @@ import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-prompt = "how to make a car "
 
-response = openai.Completion.create(
-    engine="text-davinci-002",
-    prompt = prompt,
-    temperature = 0.4,
-    max_tokens = 64
+def generate_text(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.4,
+        max_tokens=64
+    )
+    if response.status != 200:
+        raise Exception("Failed to generate text: " + response.message)
+    return response.choices[0].text.strip()
 
-)
+
+def main():
+    prompt = "how to make a car"
+    try:
+        text = generate_text(prompt)
+        print(text)
+    except Exception as ex:
+        print("Error: " + str(ex))
 
 
-print(response)
+if __name__ == "__main__":
+    main()
